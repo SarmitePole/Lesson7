44bc350cc25a325251713cc5e42ec6e5b00614ea
student <sa.pole@inbox.lv>
2019-11-09 11:41:35 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log(21); //integer (int)
+console.log(3.14); //float
+console.log(typeof 13);
+console.log(typeof 13.14);
+
+console.log('This is BOOTCAMP'); //string OK
+console.log("This is BOOTCAMP"); //string NOT OK - var gadīties sintakses kļūda, kad liks iekā a=href ""
+console.log(`This is BOOTCAMP`); //string BACK TICK izmaknto ES6
+console.log(''); //string, empty string, buleanā redzucēsies uz true
+
+console.log(true); //bool
+console.log(false); //bool
+console.log(typeof false); 
+console.log(typeof false); 
+
+console.log(null); //null;
+console.log(typeof null); 
+
+console.log([1, 2, 3, 4, 5]); //array
+console.log([1,2,3,4,5]); //array
+console.log(typeof[1,2,3,4,5]); //parāda "masīvs" = "object"
+
+var someNumber = 123;
+console.log(typeof 123);
+console.log(someNumber); 
+console.log(typeof foo);
